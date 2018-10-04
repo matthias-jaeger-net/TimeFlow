52f7fc0c4e238194aa592f5629047101a3592a90
Matthias Jäger <chromos@Matthiass-MacBook-Pro.local>
2018-10-04 16:33:14 +0200

    Grid of svg

--- index.js
+++ index.js
@@ -1,30 +1,67 @@
-const width = window.innerWidth;
-const height = window.innerHeight;
-const scale = 200;
-const rows = Math.floor(width / scale);
-const cols = Math.floor(height / scale);
+/* *
+ *  TimeFlow
+ *
+ *  This program creates an abstract graphic, based on the given Time.
+ *  Simply copy and paste the graphic, because it's rendered as a
+ *  fully scalable vector graphic. Also feel free to modify and change
+ *  this code to fit your project.
+ *
+ *  Github: https://github.com/matthias-jaeger-net/TimeFlow
+ *
+ *  Matthias Jäger, Graz 2018
+ */
 
-const Circle = function(d) {
-  let circle = '';
-  let circleR = d / 2;
-  let circleD = d;
-  circle += '<svg width="' + circleD + '" height="' + circleD + '" version="1.1" xmlns="http://www.w3.org/2000/svg">';
-  circle += '<circle cx="' + circleR + '" cy="' + circleR + '" r="' + circleR + '"';
-  circle += ' stroke="black" stroke-width="1" fill="black"/>';
-  circle += '</svg>';
-  return circle;
-}
+(function TimeFlow() {
 
-let rendering = '';
-for (let row = 0; row < rows; row++) {
-  for (let col = 0; col < cols; col++) {
-    let circleToAdd = Circle(scale);
-    rendering += circleToAdd;
+  const getCurrentTime = function() {
+    return new Date();
+  };
+  const formatTime = function(date) {
+    let now = date;
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    return [hours, minutes, seconds];
+  };
+  const queryTime = function() {
+    console.log(formatTime(getCurrentTime()));
   }
-}
+  setInterval(queryTime, 1000);
 
-console.log(window.innerWidth);
 
-console.log(rows);
 
-document.write(rendering);
+  const scale = 100;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const rows = Math.floor(width / scale);
+  const cols = Math.floor(height / scale);
+
+
+
+  // Prepare full screen svg as string
+  let renderedString = '<svg width="' + width + '" height="' + height;
+  renderedString += '" version="1.1" xmlns="http://www.w3.org/2000/svg">';
+
+  const Circle = function(x, y, r) {
+    let circle = '';
+    let circleX = x;
+    let circleY = y;
+    let circleR = r;
+    circle += '<circle cx="' + circleX + '" cy="' + circleY + '" r="' + circleR + '"';
+    circle += ' stroke="black" stroke-width="1" fill="transparent"/>';
+    return circle;
+  }
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      let x = row * scale + (scale / 2);
+      let y = col * scale + (scale / 2);
+      let circleToAdd = Circle(x, y, (scale / 2) - 5);
+      renderedString += circleToAdd;
+    }
+  }
+
+  // Close svg string and write
+  renderedString += '</svg>';
+  document.write(renderedString);
+})();
