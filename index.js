415e1ab7f728628c24b48bf522d74403b25d437b
Matthias Jäger <write@matthias-jaeger.net>
2018-10-06 09:48:40 +0200

    Refacture to a clock function, multiple clocks rendered in the same svg

--- index.js
+++ index.js
@@ -13,57 +13,78 @@
 
 (function TimeFlow() {
 
-	const scale = 100;
 	const width = window.innerWidth;
 	const height = window.innerHeight;
 
-	const getCurrentTime = function() {
-		return new Date();
+	const map = (num, in_min, in_max, out_min, out_max) => {
+		return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 	};
 
-	const formatTime = function(date) {
+	const getCurrentTime = () => {
+		let date = new Date();
 		let hours = date.getHours();
 		let minutes = date.getMinutes();
 		let seconds = date.getSeconds();
-		return [hours, minutes, seconds];
-	};
-
-	const queryTime = function() {
-		console.log(formatTime(getCurrentTime()));
-	};
-
-	const renderTimeFlow = function() {
-		// Prepare full screen svg as string
-		let renderedString = '<svg width="' + width + '" height="' + height;
-		renderedString += '" version="1.1" xmlns="http://www.w3.org/2000/svg">';
-		const Circle = function(x, y, r) {
-			let circle = '';
-			let circleX = x;
-			let circleY = y;
-			let circleR = r;
-			circle += '<circle cx="' + circleX + '" cy="' + circleY + '" r="' + circleR + '"';
-			circle += ' stroke="black" stroke-width="1" fill="transparent"/>';
-			return circle;
+		return {
+			hours: hours,
+			minutes: minutes,
+			seconds: seconds
 		}
-		const rows = Math.floor(width / scale);
-		const cols = Math.floor(height / scale);
-		for (let row = 0; row < rows; row++) {
-			for (let col = 0; col < cols; col++) {
-				let x = row * scale + (scale / 2);
-				let y = col * scale + (scale / 2);
-				let circleToAdd = Circle(x, y, (scale / 2) - 5);
-				renderedString += circleToAdd;
-			}
+	};
+
+	const CircleString = (position, radius) => {
+		let circle = '';
+		circle += '<circle cx="' + position.x + '" cy="' + position.y + '" r="' + radius + '"';
+		circle += ' stroke="black" stroke-width="8" fill="transparent"/>';
+		return circle;
+	};
+
+	const HandString = (position, angle, radius) => {
+		let x1 = position.x;
+		let y1 = position.y;
+		let x2 = x1 + Math.cos(angle - Math.PI / 2) * radius;
+		let y2 = y1 + Math.sin(angle - Math.PI / 2) * radius;
+		let style = 'stroke:black; stroke-width:8; '
+		return '<line stroke-linecap="round" x1="' + x1 + '" y1="' + y1 + '" x2="' + x2 + '" y2="' + y2 + '" style="' + style + '" />';
+	}
+
+	const createClockString = (position, time, radius) => {
+		let unit = radius / 100.0;
+		let clock = CircleString(position, radius);
+		clock += HandString(position, map(time.hours, 0, 24, 0, Math.PI * 4), unit * 60);
+		clock += HandString(position, map(time.minutes, 0, 59, 0, Math.PI * 2), unit * 70);
+		clock += HandString(position, map(time.seconds, 0, 59, 0, Math.PI * 2), unit * 80);
+		return clock;
+	};
+
+	const createClockGrid = () => {
+		console.log('yh');
+	}
+
+	const writeClockString = () => {
+		let position = {
+			x: width / 2,
+			y: height / 2
 		}
-		// Close svg string and write
-		renderedString += '</svg>';
-		document.write(renderedString);
+		let clock1 = createClockString(position, getCurrentTime(), 100);
+
+		let position2 = {
+			x: width / 3,
+			y: height / 5
+		}
+		let clock2 = createClockString(position2, getCurrentTime(), 200);
+
+		let grafic = '<svg width="' + width + '" height="' + height + '" version="1.1" xmlns="http://www.w3.org/2000/svg">';
+		grafic += clock1 + " " + clock2;
+		grafic += '</svg>';
+
+		document.open();
+		document.write(grafic);
 	};
 
-	const animationLoop = function() {
-		queryTime();
-		renderTimeFlow();
-	};
 
-	setInterval(animationLoop, 1000);
+
+
+	setInterval(writeClockString, 1000);
+
 })();
